dc31522e1a57fae419c72eff5e0c2533d35688a2
乖小咩 <guaixiaomie@gmail.com>
2026-03-30 17:10:31 +0800

    Delete ChatArchiveTool_github directory

--- ChatArchiveTool_github/extractors/base.py
+++
@@ -1,8 +0,0 @@
-class BaseExtractor:
-    name = "base"
-
-    def detect(self, input_path: str) -> bool:
-        return False
-
-    def extract(self, input_path: str, output_dir: str):
-        raise NotImplementedError
